6eeaa4bc8b0a52a3e4ff3b77732d467e14a8efb7
buns-li <buns.zpli@gmail.com>
2019-11-29 20:30:15 +0800

    refactor(core): 调整输出接口的代码逻辑

--- packages/core/lib/index.ts
+++ packages/core/lib/index.ts
@@ -1,51 +1,68 @@
-import { includes, isFunction } from "./utils";
-import { api, bindListener, wrap, invokeH5, notifyHost } from "./api";
-import { init, JSApi, HostInvokePayload, ArgsTransform } from "./actions/index";
+import { includes } from "./utils";
+import { api, bindListener, wrap, invokeH5, notifyHost, _ } from "./api";
+import {
+	init,
+	JSApi,
+	HostInvokePayload,
+	JSApiFuncCallHost,
+	JSApiFuncOn,
+	JSApiFuncInvokeH5,
+	JSApiFuncWrap,
+	JSApiFuncWrapHandler,
+	JSApiFuncNotifyHost
+} from "./actions/index";
 
-const ForbiddenActions = ["on", "wrap", "invokeH5", "config", "ready", "error", "callHost"];
+const ForbiddenActions = ["on", "wrap", "invokeH5", "error", "debug"];
 
 /**
  * 提供给外部重载
  */
-wrap("callHost", (payload: HostInvokePayload): void => {
+wrap<JSApiFuncCallHost>("callHost", (payload: HostInvokePayload): void => {
 	window.JSBridge
-		? window.JSBridge.invoke(payload.action, payload.data, payload.callbackid)
+		? window.JSBridge.invoke && window.JSBridge.invoke(payload.action, payload.data, payload.callbackid)
 		: window.webkit &&
 		  window.webkit.messageHandlers &&
 		  window.webkit.messageHandlers.JSBridge &&
+		  window.webkit.messageHandlers.JSBridge.postMessage &&
 		  window.webkit.messageHandlers.JSBridge.postMessage(payload);
 });
 
+wrap<JSApiFuncOn>(
+	"on",
+	(action: string): JSApi => {
+		if (action !== "config" && action !== "ready" && action !== "error") {
+			bindListener(`on.${action}`);
+		} else {
+			invokeH5("error", {
+				code: 403,
+				msg: `Error: Forbidden "${action}" by "on"`
+			});
+		}
+		return JSApi;
+	}
+);
+
+wrap<JSApiFuncInvokeH5>("invokeH5", invokeH5);
+
+wrap<JSApiFuncWrap>(
+	"wrap",
+	(action: string, handler: JSApiFuncWrapHandler): JSApi => {
+		if (includes(ForbiddenActions, action)) {
+			invokeH5("error", {
+				code: 403,
+				msg: `Error: Forbidden wrap "${action}"`
+			});
+		} else {
+			const fn = handler(notifyHost as JSApiFuncNotifyHost);
+			fn && wrap(action, fn);
+		}
+		return JSApi;
+	}
+);
+
 const JSApi = api as JSApi;
 
-JSApi.on = function(action: string): JSApi {
-	if (includes(ForbiddenActions, action)) {
-		JSApi.invokeH5(
-			"error",
-			JSON.stringify({
-				code: 403,
-				msg: `Error: Forbidden ${action}`
-			})
-		);
-	} else {
-		bindListener(`on.${action}`);
-	}
-
-	return JSApi;
-};
-
-JSApi.wrap = function wrapFn(action: string, argsTransform?: ArgsTransform): JSApi {
-	wrap(action, (...args: any[]): void => {
-		let cb;
-		if (args && args.length && isFunction(args[args.length - 1])) {
-			cb = args.pop() as Function;
-		}
-		notifyHost(action, args && args.length && argsTransform ? argsTransform(...args) : {}, cb);
-	});
-	return JSApi;
-};
-
-JSApi.invokeH5 = invokeH5;
+JSApi._ = _;
 
 init();
 
