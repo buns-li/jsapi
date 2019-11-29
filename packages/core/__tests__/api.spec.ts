ae8e3216430f71f2ae51e8da1663cdd16a77d7ab
buns-li <buns.zpli@gmail.com>
2019-11-29 20:26:41 +0800

    test(core): 调整单元测试案例

--- packages/core/__tests__/api.spec.ts
+++ packages/core/__tests__/api.spec.ts
@@ -1,8 +1,6 @@
 "use strict";
 
-// @ts-nocheck
-
-import JSApi, { HostInvokePayload, FeedbackMsgInfo } from "../lib/index";
+import JSApi, { HostInvokePayload, FeedbackMsgInfo, JSApiFuncNotifyHost } from "../lib/index";
 
 describe("JSApi", () => {
 	let payloadData: HostInvokePayload;
@@ -23,83 +21,44 @@ describe("JSApi", () => {
 			errInfo = err;
 		});
 
-		JSApi.debug = <T>(action: string, data?: T, err?: Error): void => {
+		JSApi.debug = (action: string, data?: any, err?: Error): void => {
 			debugInfo = { action, data, err };
 		};
 	});
 
 	afterEach(() => {
 		debugInfo = void 0;
+		if (payloadData) {
+			payloadData.action = "";
+			payloadData.data = "";
+			payloadData.callbackid = "";
+		}
 	});
 
-	it("H5 notify App to do something", () => {
-		const obj = { url: "http://www.baidu.com", title: "百度", query: {} };
-
-		JSApi.view.goto(obj.url, obj.title, obj.query);
-
-		expect(payloadData).not.toBeUndefined();
-		expect(payloadData.action).toBe("view.goto");
-		expect(payloadData.data).toBe(JSON.stringify(obj));
-	});
-
-	it("App notify H5 to do something", done => {
-		JSApi.onViewRefresh({
-			success(data) {
-				expect(data).toBe("ok");
-				done();
-			}
-		});
-		JSApi.invokeH5("view.refresh", "ok");
-	});
-
-	it("notify App do something with callback", done => {
-		JSApi.view.changeTitle("新的标题", (res: string) => {
-			expect(res).toBe("change:ok");
-			done();
-		});
-
-		const data = JSON.parse(payloadData.data);
-
-		expect(data).toHaveProperty("title");
-		expect(data.title).toBe("新的标题");
-
-		JSApi.invokeH5(payloadData.callbackid, "change:ok");
-	});
-
-	it("notify App do something with callback : object params ", done => {
-		JSApi.view.changeTitle("新的标题", (res: any) => {
-			expect(res).toHaveProperty("title");
-			expect(res.title).toBe("新的标题");
-			done();
-		});
-
-		JSApi.invokeH5(payloadData.callbackid, payloadData.data);
-	});
-
-	it(" `JSApi.on` ", done => {
-		JSApi.on("wrap");
+	it(" `JSApi.wrap` with forbidden action", () => {
+		JSApi.wrap("wrap", () => void 0);
 		expect(errInfo).not.toBeUndefined();
 		expect(errInfo.code).toBe(403);
-
-		JSApi.on("unit.test");
-		expect(JSApi).toHaveProperty("onUnitTest");
-
-		JSApi.onUnitTest({
-			success(data: any) {
-				expect(data).toBe("ok");
-				done();
-			}
-		});
-		JSApi.invokeH5("unit.test", "ok");
 	});
 
-	it(" `JSApi.on` with '.'", () => {
-		JSApi.on(".");
-		expect(JSApi).not.toHaveProperty(".");
+	it(" `JSApi.wrap` with action='' ", () => {
+		JSApi.wrap("", () => void 0);
+		expect(payloadData).toBeUndefined();
+
+		JSApi.wrap("", (notifyHost: JSApiFuncNotifyHost) => {
+			return (text: string): void => {
+				notifyHost("", { text });
+			};
+		});
+		expect(payloadData).toBeUndefined();
 	});
 
 	it(" `JSApi.wrap` ", done => {
-		JSApi.wrap("unit.test", (text: string) => ({ text }));
+		JSApi.wrap("unit.test", (notifyHost: JSApiFuncNotifyHost):
+			| Function
+			| undefined => (text: string, cb?: Function): void => {
+			notifyHost("unit.test", { text }, cb);
+		});
 
 		JSApi.unit.test();
 		expect(payloadData.action).toBe("unit.test");
@@ -111,8 +70,14 @@ describe("JSApi", () => {
 			done();
 		});
 		JSApi.invokeH5(payloadData.callbackid, "");
+	});
 
-		JSApi.wrap("chain.unit.test", (text: string) => ({ text }));
+	it(" `JSApi.wrap` with long action", done => {
+		JSApi.wrap("chain.unit.test", (notifyHost: JSApiFuncNotifyHost):
+			| Function
+			| undefined => (text: string, cb?: Function): void => {
+			notifyHost("chain.unit.test", { text }, cb);
+		});
 
 		JSApi.chain.unit.test();
 		expect(payloadData.action).toBe("chain.unit.test");
@@ -123,7 +88,7 @@ describe("JSApi", () => {
 		JSApi.chain.unit.test("message", () => {
 			done();
 		});
-		JSApi.invokeH5(payloadData.callbackid, "");
+		JSApi.invokeH5(payloadData.callbackid);
 	});
 
 	it(" `JSApi.debug` ", () => {
@@ -175,5 +140,81 @@ describe("JSApi", () => {
 
 		JSApi.invokeH5("");
 		expect(debugInfo).toBeUndefined();
+	});
+
+	it(" `JSApi.on` ", done => {
+		JSApi.on("unit.test");
+		expect(JSApi).toHaveProperty("onUnitTest");
+
+		JSApi.onUnitTest({
+			success(data: any) {
+				expect(data).toBe("ok");
+				done();
+			}
+		});
+		JSApi.invokeH5("unit.test", "ok");
+	});
+
+	it(" `JSApi.on` with '.'", () => {
+		JSApi.on(".");
+		expect(JSApi).not.toHaveProperty(".");
+	});
+
+	it(" `JSApi.on` with '403'", () => {
+		JSApi.on("config");
+		expect(errInfo).not.toBeUndefined();
+		expect(errInfo.code).toBe(403);
+
+		JSApi.on("ready");
+		expect(errInfo).not.toBeUndefined();
+		expect(errInfo.code).toBe(403);
+
+		JSApi.on("error");
+		expect(errInfo).not.toBeUndefined();
+		expect(errInfo.code).toBe(403);
+	});
+
+	it("H5 notify App to do something", () => {
+		const obj = { url: "http://www.baidu.com", title: "百度", query: {} };
+
+		JSApi.view.goto(obj.url, obj.title, obj.query);
+
+		expect(payloadData).not.toBeUndefined();
+		expect(payloadData.action).toBe("view.goto");
+		expect(payloadData.data).toBe(JSON.stringify(obj));
+	});
+
+	it("App notify H5 to do something", done => {
+		JSApi.onViewRefresh({
+			success(data) {
+				expect(data).toBe("ok");
+				done();
+			}
+		});
+		JSApi.invokeH5("view.refresh", "ok");
+	});
+
+	it("notify App do something with callback", done => {
+		JSApi.view.changeTitle("新的标题", (res: string) => {
+			expect(res).toBe("change:ok");
+			done();
+		});
+
+		const data = JSON.parse(payloadData.data);
+
+		expect(data).toHaveProperty("title");
+		expect(data.title).toBe("新的标题");
+
+		JSApi.invokeH5(payloadData.callbackid, "change:ok");
+	});
+
+	it("notify App do something with callback : object params ", done => {
+		JSApi.view.changeTitle("新的标题", (res: any) => {
+			expect(res).toHaveProperty("title");
+			expect(res.title).toBe("新的标题");
+			done();
+		});
+
+		JSApi.invokeH5(payloadData.callbackid, payloadData.data);
 	});
 });
---
+++ packages/core/__tests__/callhost.spec.ts
@@ -0,0 +1,23 @@
+"use strict";
+
+import JSApi, { HostInvokePayload } from "../lib/index";
+
+describe("JSApi.callHost", () => {
+	let payloadData: HostInvokePayload;
+	beforeAll(() => {
+		window.JSBridge = {
+			invoke(action: string, data: string, callbackid: string): void {
+				payloadData = {
+					action,
+					data,
+					callbackid
+				};
+			}
+		};
+	});
+
+	it(" `test JSBridge` ", () => {
+		JSApi.view.back();
+		expect(payloadData.action).toBe("view.back");
+	});
+});
